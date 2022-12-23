1d23b6825e1b9f0f0115a22194b30907aa4a93f0
phi <postapiano@gmail.com>
2022-12-23 16:15:07 +0000

    JavaScriptmas 23

---
+++ JavaScriptmas 2022/23 - Holiday Gift Shopping/index.js
@@ -0,0 +1,42 @@
+import products from "./data.js";
+
+/*
+    You're online shopping for holiday gifts, but money is tight
+    so we need to look at the cheapest items first. 
+    Use the built in sort() method to write a function that returns a new array of
+    products sorted by price, cheapest to most expensive. 
+    
+    Then log the item and the price to the console: 
+    
+    💕,0
+    🍬,0.89
+    🍫,0.99
+    🧁,0.99
+    📚,0.99
+    ... continued
+*/
+
+function sortProducts(data){
+    // return data.sort((a, b) => a.price - b.price)
+    return data.sort((a, b) => a.price - b.price)
+}
+
+const listByCheapest = sortProducts(products);
+listByCheapest.map(e => console.log(e.product, e.price));
+
+
+(() => {
+    const tags = listByCheapest.map(product => {
+        return `
+            <div class="prodContainer">
+                <p class="products">${product.product}</p>
+                <p>${product.price ? '£' + product.price : 'free!!!'}</p>
+            </div>
+        `
+    })
+    
+    document.body.innerHTML += tags.join('')
+    
+})()
+
+
